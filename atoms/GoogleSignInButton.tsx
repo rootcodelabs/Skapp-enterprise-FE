7e63015e8c10d206587f17564f3d372dd8009a48
ThinuwanW <thinuwan.wickramaarachchi@rootcode.io>
2024-08-30 09:16:01 +0530

    prettier auto correct

--- atoms/GoogleSignInButton.tsx
+++ atoms/GoogleSignInButton.tsx
@@ -1,11 +1,9 @@
-
-
 export default function GoogleSignInButton() {
   return (
-    <div style={{textAlign: 'center', marginTop: '20px'}}>
-    <button style={{borderRadius: '10px', background: 'lightblue'}}>
-      Google SignIn
-    </button>
+    <div style={{ textAlign: "center", marginTop: "20px" }}>
+      <button style={{ borderRadius: "10px", background: "lightblue" }}>
+        Google SignIn
+      </button>
     </div>
   );
 }
--- organisms/SignInWidget.tsx
+++ organisms/SignInWidget.tsx
@@ -1,17 +1,15 @@
-import SignInForm from "@/community/molecules/SignInForm";
-import GoogleSignInButton from "@/enterprise/atoms/GoogleSignInButton";
-
+import SignInForm from "~community/molecules/SignInForm";
+import GoogleSignInButton from "~enterprise/atoms/GoogleSignInButton";
 
 export default function SignInWidget() {
-
   const signInButtonClick = () => {
-    alert('Enterprise sign in trigger...')
-  }
+    alert("Enterprise sign in trigger...");
+  };
 
   return (
     <div>
-      <SignInForm click={signInButtonClick}/>
-      <GoogleSignInButton/>
+      <SignInForm click={signInButtonClick} />
+      <GoogleSignInButton />
     </div>
   );
 }
